e5fc9ecaff0b54a2a595019626a036ecc7b6da06
Armando-coder-gif <armando.gonzalez@correo.unimet.edu.ve>
2025-05-05 14:22:00 -0600

    optional and named parameters

--- 05_functs_params.dart
+++ 05_functs_params.dart
@@ -1,6 +1,7 @@
 void main() {
   print(greetEveryone());
   print('suma: ${addTwoNumbers(10, 20)}');
+  print('${greetPerson( name: 'Armando', message: 'Hi,' )}');
 }
 
 // String greetEveryone() {
@@ -20,4 +21,8 @@ String greetEveryone() => 'Hello everyone!';
 int addTwoNumbers(int a, [int b = 0]) {
   // b is optional and defaults to 0
   return a + b;
+}
+
+greetPerson({ required String name, String message = 'Hola,'}){ // parametros opcionales y nombrados
+  return '$message $name';
 }

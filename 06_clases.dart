610fd3c8045b25c90bbb01670aa7cd4019b46c9a
Armando-coder-gif <armando.gonzalez@correo.unimet.edu.ve>
2025-05-05 16:16:09 -0600

    override

--- 06_clases.dart
+++ 06_clases.dart
@@ -1,6 +1,6 @@
 void main() {
-  final Hero spiderMan = Hero("spiderMan", "web-slinging");
-  print(spiderMan);
+  final Hero spiderMan = Hero(name: "spiderMan", power: "web-slinging");
+  print(spiderMan.toString());
   print(spiderMan.name);
   print(spiderMan.power);
 }
@@ -9,6 +9,14 @@ class Hero {
   String name;
   String power;
 
-  Hero(this.name, this.power); // Constructor with positional parameters
+  Hero({
+    required this.name,
+    required this.power,
+  }); // Constructor with positional parameters
   // Hero(String pName, String pPower) : name = pName, power = pPower;
+
+  @override
+  toString() {
+    return "$name - $power";
+  }
 }

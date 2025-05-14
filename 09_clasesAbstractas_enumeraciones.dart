3f4f8b3fac28f60d785e3c4679b528ceefd01eb3
Armando-coder-gif <armando.gonzalez@correo.unimet.edu.ve>
2025-05-14 15:30:18 -0600

    extends

--- 09_clasesAbstractas_enumeraciones.dart
+++ 09_clasesAbstractas_enumeraciones.dart
@@ -1,24 +1,34 @@
 void main() {
-  // final windPlant = EnergyPlant();
+  final windPlant = WindPlant(initialEnergy: 100);
+
+  print('wind: ${chargePhone(windPlant)}');
 }
 
-
-enum EnergyType{
-  nuclear,
-  wind,
-  water
+double chargePhone(EnergyPlant plant) {
+  if (plant.energyLeft < 10) {
+    throw Exception("not enough energy");
+  }
+  return plant.energyLeft - 10;
 }
+
+enum EnergyType { nuclear, wind, water }
 
 abstract class EnergyPlant {
   double energyLeft;
   EnergyType type; // nuclear, wind, water
 
-  EnergyPlant({
-    required this.energyLeft,
-    required this.type,
-  });
+  EnergyPlant({required this.energyLeft, required this.type});
 
+  void consumeEnergy(double amount) {}
+}
+
+// extends or implements
+class WindPlant extends EnergyPlant {
+  WindPlant({required double initialEnergy})
+    : super(energyLeft: initialEnergy, type: EnergyType.wind);
+
+  @override
   void consumeEnergy(double amount) {
-
+    energyLeft -= amount;
   }
 }
